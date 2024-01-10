1f06174450343996371c20c58aeeec6042ea9180
MartinHolts <martinholtsmeier@gmail.com>
2024-01-10 17:16:50 +0200

    Got GPT-2 model working.

--- blip-2.py
+++ blip-2.py
@@ -1,20 +1,34 @@
-from transformers import (
- Blip2VisionConfig,
- Blip2QFormerConfig,
- OPTConfig,
- Blip2Config,
- Blip2ForConditionalGeneration,
-)
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import torch
+from PIL import Image
 
-# Initializing BLIP-2 vision, BLIP-2 Q-Former and language model configurations
-vision_config = Blip2VisionConfig().to_dict()
-qformer_config = Blip2QFormerConfig().to_dict()
-text_config = OPTConfig().to_dict()
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
-# Combine the configurations into a single dictionary
-combined_config = {**vision_config, **qformer_config, **text_config}
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
-# Initialize a Blip2Config with the combined configuration
-config = Blip2Config(**combined_config)
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+def predict_caption(image_paths):
+    images = []
+    for image_path in image_paths:
+        i_image = Image.open(image_path)
+        if i_image.mode != "RGB":
+            i_image = i_image.convert(mode="RGB")
+        images.append(i_image)
 
-print(combined_config)
+    pixel_values = feature_extractor(
+        images=images, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    print("Final Caption is", preds)
+    return preds
+
+predict_caption(['image.jpg'])
