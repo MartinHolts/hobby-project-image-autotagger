02c968469d7936c87e0883cc3307a2904ce955ce
MartinHolts <martinholtsmeier@gmail.com>
2024-01-21 22:39:34 +0200

    Imported model under blip2 python version.

--- blip2.py
+++ blip2.py
@@ -22,3 +22,10 @@ raw_image.resize((596, 437))
 
 # setup device to use CPU
 device = "cpu"
+
+# 2.7 Coco
+model, vis_processors, _ = load_model_and_preprocess(
+    name="blip2_opt", model_type="caption_coco_opt2.7b", is_eval=True, device=device
+)
+
+vis_processors.keys()
