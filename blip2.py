78c49e07ac809a7be634c7a1016e56cc6fcfd7e1
MartinHolts <martinholtsmeier@gmail.com>
2024-01-21 22:47:36 +0200

    Added  image prepartion part.

--- blip2.py
+++ blip2.py
@@ -23,9 +23,13 @@ raw_image.resize((596, 437))
 # setup device to use CPU
 device = "cpu"
 
+# Load pretrained/finetuned BLIP2 captioning model
 # 2.7 Coco
 model, vis_processors, _ = load_model_and_preprocess(
     name="blip2_opt", model_type="caption_coco_opt2.7b", is_eval=True, device=device
 )
 
 vis_processors.keys()
+
+# prepare the image as model input using the associated processors
+image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
