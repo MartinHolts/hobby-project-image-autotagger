b48d5488941bffa846c2e91ccc4d458d1c903df2
MartinHolts <martinholtsmeier@gmail.com>
2024-01-21 22:30:48 +0200

    Fixed import issues and added image import to blip2 python.

--- blip2.py
+++ blip2.py
@@ -1,5 +1,6 @@
 import pkg_resources
 import subprocess
+import sys
 
 try:
     pkg_resources.get_distribution('salesforce-lavis')
@@ -7,3 +8,13 @@ try:
 except pkg_resources.DistributionNotFound:
     print("Installing salesforce-lavis...")
     subprocess.check_call([sys.executable, "-m", "pip", "install", "salesforce-lavis"])
+
+
+import torch
+from PIL import Image
+import requests
+from lavis.models import load_model_and_preprocess
+
+img_path = './image.jpg' # Path to the image file
+raw_image = Image.open(img_path).convert('RGB')
+raw_image.resize((596, 437))
