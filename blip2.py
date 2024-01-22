d2acc8ff0f3d0af5bc849ad2c6329f7eefd0723c
MartinHolts <martinholtsmeier@gmail.com>
2024-01-22 18:55:57 +0200

    Added tags examples to blip2.

--- blip2.py
+++ blip2.py
@@ -20,20 +20,38 @@ img_path = './image.jpg'
 raw_image = Image.open(img_path).convert('RGB')
 raw_image.resize((596, 437))
 
-# setup device to use CPU
+# Setup device to use CPU
 device = "cpu"
 
-# Load pretrained/finetuned BLIP2 captioning model
-# 2.7 Coco
+""" Load pretrained/finetuned BLIP2 captioning model
+ 2.7 Coco
 model, vis_processors, _ = load_model_and_preprocess(
     name="blip2_opt", model_type="caption_coco_opt2.7b", is_eval=True, device=device
+)"""
+
+# Load pretrained/finetuned BLIP2 captioning model
+# 2.7B Pretrain
+model, vis_processors, _ = load_model_and_preprocess(
+    name="blip2_opt", model_type="pretrain_opt2.7b", is_eval=True, device=device
 )
+print("Loaded normal 2.7b model into RAM")
 
 vis_processors.keys()
 
-# prepare the image as model input using the associated processors
+# Prepare the image as model input using the associated processors
 image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+print("Prepared image for model")
 
-# generate caption using beam search
+# Generate caption using beam search
 caption = model.generate({"image": image})
 print(caption)
+
+# Generate tags
+tags = model.generate({"image": image, "prompt": "Generate 20 tags for this image, separated by commas."})
+print(tags)
+
+tags2 = model.generate({"image": image, "prompt": "Question: which city is this? Answer:"})
+print(tags2)
+
+tags3 = model.generate({"image": image, "prompt": "Describe the image."})
+print(tags3)
