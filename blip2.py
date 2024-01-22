370c716c48e6e3e43c5c9c92dc1120d59db3d848
MartinHolts <martinholtsmeier@gmail.com>
2024-01-22 21:58:38 +0200

    Tried out more models for generating tags.

--- blip2.py
+++ blip2.py
@@ -16,25 +16,51 @@ import requests
 from lavis.models import load_model_and_preprocess
 
 # Import image file and convert to RGB and resize
-img_path = './image.jpg'
+img_path = './image2.jpg'
 raw_image = Image.open(img_path).convert('RGB')
 raw_image.resize((596, 437))
 
 # Setup device to use CPU
 device = "cpu"
 
-""" Load pretrained/finetuned BLIP2 captioning model
- 2.7 Coco
-model, vis_processors, _ = load_model_and_preprocess(
-    name="blip2_opt", model_type="caption_coco_opt2.7b", is_eval=True, device=device
-)"""
+# we associate a model with its preprocessors to make it easier for inference.
 
-# Load pretrained/finetuned BLIP2 captioning model
+# COCO
+# 2.7 Coco
+# Tried. Ran 4 minutes. Couldn't generate great tags.
+#model, vis_processors, _ = load_model_and_preprocess(
+#    name="blip2_opt", model_type="caption_coco_opt2.7b", is_eval=True, device=device
+#)
+# 6.7 Coco
+# model, vis_processors, _ = load_model_and_preprocess(
+#     name="blip2_opt", model_type="caption_coco_opt6.7b", is_eval=True, device=device
+# )
+# 5XL Coco
+# model, vis_processors, _ = load_model_and_preprocess(
+#     name="blip2_t5", model_type="caption_coco_flant5xl", is_eval=True, device=device
+# )
+
+# PRETRAIN
 # 2.7B Pretrain
-model, vis_processors, _ = load_model_and_preprocess(
-    name="blip2_opt", model_type="pretrain_opt2.7b", is_eval=True, device=device
-)
-print("Loaded normal 2.7b model into RAM")
+# Tried. Ran 4 minutes. Generated longer caption than Coco. Didn't generate tags.
+# model, vis_processors, _ = load_model_and_preprocess(
+#     name="blip2_opt", model_type="pretrain_opt2.7b", is_eval=True, device=device
+# )
+# 6.7B Pretrain
+# TRIED. Ran 4 hours.
+# model, vis_processors, _ = load_model_and_preprocess(
+#     name="blip2_opt", model_type="pretrain_opt6.7b", is_eval=True, device=device
+# )
+# 5XL Pretrain
+# TRIED. Ran 10 minutes. Maybe faster on patch of images. Since runs checkpoints shards every time. Generated terrible captions.
+# model, vis_processors, _ = load_model_and_preprocess(
+#    name="blip2_t5", model_type="pretrain_flant5xl", is_eval=True, device=device
+# )
+# 5XXL Pretrain
+# TRIED. Didn't run
+# model, vis_processors, _ = load_model_and_preprocess(
+#     name="blip2_t5", model_type="pretrain_flant5xxl", is_eval=True, device=device
+# )
 
 vis_processors.keys()
 
@@ -47,11 +73,5 @@ caption = model.generate({"image": image})
 print(caption)
 
 # Generate tags
-tags = model.generate({"image": image, "prompt": "Generate 20 tags for this image, separated by commas."})
+tags = model.generate({"image": image, "prompt": "Question: What words describe this image?"})
 print(tags)
-
-tags2 = model.generate({"image": image, "prompt": "Question: which city is this? Answer:"})
-print(tags2)
-
-tags3 = model.generate({"image": image, "prompt": "Describe the image."})
-print(tags3)
